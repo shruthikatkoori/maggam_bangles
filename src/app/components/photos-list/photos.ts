8231f492c9043195de87e0b0608db3a032c08cf4
shruthikatkoori <shruthi394@gmail.com>
2021-02-02 11:48:39 +0530

    JSON update

--- src/app/components/photos-list/photos.ts
+++ src/app/components/photos-list/photos.ts
@@ -2,281 +2,337 @@ export const photos= [
     {
         "title": "Maggam Bangles",
         "imagePath": "5.jpg",
-        "Code": "1901"
+        "Code": "1901",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "6.jpg",
-        "Code": "1902"
+        "Code": "1902",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "7.jpg",
-        "Code": "1903"
+        "Code": "1903",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "8.jpg",
-        "Code": "1904"
+        "Code": "1904",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "9.jpg",
-        "Code": "1905"
+        "Code": "1905",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "10.jpg",
-        "Code": "1906"
+        "Code": "1906",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "11.jpg",
-        "Code": "1907"
+        "Code": "1907",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "12.jpg",
-        "Code": "1908"
+        "Code": "1908",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "13.jpg",
-        "Code": "1909"
+        "Code": "1909",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "14.jpg",
-        "Code": "1910"
+        "Code": "1910",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "15.jpg",
-        "Code": "1911"
+        "Code": "1911",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "16.jpg",
-        "Code": "1912"
+        "Code": "1912",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "17.jpg",
-        "Code": "1913"
+        "Code": "1913",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "18.jpg",
-        "Code": "1914"
+        "Code": "1914",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "19.jpg",
-        "Code": "1915"
+        "Code": "1915",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "20.jpg",
-        "Code": "1916"
+        "Code": "1916",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "21.jpg",
-        "Code": "1917"
+        "Code": "1917",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "22.jpg",
-        "Code": "1918"
+        "Code": "1918",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "23.jpg",
-        "Code": "1919"
+        "Code": "1919",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "24.jpg",
-        "Code": "1920"
+        "Code": "1920",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "25.jpg",
-        "Code": "1921"
+        "Code": "1921",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "26.jpg",
-        "Code": "1922"
+        "Code": "1922",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "27.jpg",
-        "Code": "1923"
+        "Code": "1923",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "28.jpg",
-        "Code": "1924"
+        "Code": "1924",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "29.jpg",
-        "Code": "1925"
+        "Code": "1925",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "30.jpg",
-        "Code": "1926"
+        "Code": "1926",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "31.jpg",
-        "Code": "1927"
+        "Code": "1927",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "32.jpg",
-        "Code": "1928"
+        "Code": "1928",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "33.jpg",
-        "Code": "1929"
+        "Code": "1929",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "34.jpg",
-        "Code": "1930"
+        "Code": "1930",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "35.jpg",
-        "Code": "1931"
+        "Code": "1931",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "36.jpg",
-        "Code": "1932"
+        "Code": "1932",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "37.jpg",
-        "Code": "1933"
+        "Code": "1933",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "38.jpg",
-        "Code": "1934"
+        "Code": "1934",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "39.jpg",
-        "Code": "1935"
+        "Code": "1935",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "40.jpg",
-        "Code": "1936"
+        "Code": "1936",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "41.jpg",
-        "Code": "1937"
+        "Code": "1937",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "42.jpg",
-        "Code": "1938"
+        "Code": "1938",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "43.jpg",
-        "Code": "1939"
+        "Code": "1939",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "44.jpg",
-        "Code": "1940"
+        "Code": "1940",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "45.jpg",
-        "Code": "1941"
+        "Code": "1941",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "46.jpg",
-        "Code": "1942"
+        "Code": "1942",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "47.jpg",
-        "Code": "1943"
+        "Code": "1943",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "48.jpg",
-        "Code": "1944"
+        "Code": "1944",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "49.jpg",
-        "Code": "1945"
+        "Code": "1945",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "50.jpg",
-        "Code": "1946"
+        "Code": "1946",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "51.jpg",
-        "Code": "1947"
+        "Code": "1947",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "52.jpg",
-        "Code": "1948"
+        "Code": "1948",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "53.jpg",
-        "Code": "1949"
+        "Code": "1949",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "54.jpg",
-        "Code": "1950"
+        "Code": "1950",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "55.jpg",
-        "Code": "1951"
+        "Code": "1951",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "56.jpg",
-        "Code": "1952"
+        "Code": "1952",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "57.jpg",
-        "Code": "1953"
+        "Code": "1953",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "58.jpg",
-        "Code": "1954"
+        "Code": "1954",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "59.jpg",
-        "Code": "1955"
+        "Code": "1955",
+        "description": "Maggam Bangles"
     },
     {
         "title": "Maggam Bangles",
         "imagePath": "60.jpg",
-        "Code": "1956"
+        "Code": "1956",
+        "description": "Maggam Bangles"
     }
 ]
